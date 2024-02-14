39d6ac6a33f6951904a947d903f0e4e3293d2010
maws <martin.wiesner@festo.com>
2024-02-14 13:19:09 +0100

    Added unittests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## v0.2.0 - 26.01.24
 ### Added
+- Unittests for CpxBase
 - More examples
 - Added content to README.md
 - Added module functions CPX-E/AP
@@ -27,6 +28,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - CpxE: Added CLI for writing and reading values.
 
 ### Changed
+- CpxBase: now initializes with self.base=None instead of no self.base at all
 - CpxAp: Parameter read/write now utilizes multi-register access
 - CpxApEP: deleted write_parameters(), added configure_monitoring_load_supply() instead
 - Pybodmus Error now raises "ConnectionAbortedError" instead of "ValueError"
---
+++ tests/unit_tests/cpx_system/test_cpx_base.py
@@ -0,0 +1,386 @@
+"""Contains tests for CpxBase class"""
+
+from unittest.mock import Mock
+from dataclasses import dataclass
+import pytest
+
+from pymodbus.client import ModbusTcpClient
+from cpx_io.cpx_system.cpx_base import CpxBase, CpxInitError
+
+
+class TestCpxBase:
+    "Test CpxBase methods"
+
+    def test_constructor_no_ip(self):
+        "Test constructor"
+        # Arrange
+
+        # Act
+        cpx = CpxBase()
+
+        # Assert
+        assert isinstance(cpx, CpxBase)
+
+    def test_constructor_with_ip(self):
+        "Test constructor"
+        # Arrange
+
+        # Act
+        cpx = CpxBase("192.168.1.1")
+
+        # Assert
+        assert isinstance(cpx, CpxBase)
+        assert isinstance(cpx.client, ModbusTcpClient)
+
+    def test_context_manager_no_ip(self):
+        "Test context manager"
+        # Arrange
+
+        # Act
+        with CpxBase() as cpx:
+
+            # Assert
+            assert isinstance(cpx, CpxBase)
+
+    def test_context_manager_with_ip(self):
+        "Test context manager"
+        # Arrange
+
+        # Act
+        with CpxBase("192.168.1.1") as cpx:
+
+            # Assert
+            assert isinstance(cpx, CpxBase)
+            assert isinstance(cpx.client, ModbusTcpClient)
+
+    def test_bitwiseReg8_from_bytes(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg8):
+            """register dataclass"""
+
+            bit0: bool
+            bit1: bool
+            bit2: bool
+            bit3: bool
+            bit4: bool
+            bit5: bool
+            bit6: bool
+            bit7: bool
+
+        # Act
+        reg = TestRegister.from_bytes(b"\xAA")
+
+        # Assert
+        reg.bit0 = True
+        reg.bit1 = False
+        reg.bit2 = True
+        reg.bit3 = False
+        reg.bit4 = True
+        reg.bit5 = False
+        reg.bit6 = True
+        reg.bit7 = False
+
+    def test_bitwiseReg8_from_int(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg8):
+            """register dataclass"""
+
+            bit0: bool
+            bit1: bool
+            bit2: bool
+            bit3: bool
+            bit4: bool
+            bit5: bool
+            bit6: bool
+            bit7: bool
+
+        # Act
+        reg = TestRegister.from_int(0xAA)
+
+        # Assert
+        reg.bit0 = True
+        reg.bit1 = False
+        reg.bit2 = True
+        reg.bit3 = False
+        reg.bit4 = True
+        reg.bit5 = False
+        reg.bit6 = True
+        reg.bit7 = False
+
+    def test_bitwiseReg8_to_int(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg8):
+            """register dataclass"""
+
+            bit0: bool = False
+            bit1: bool = True
+            bit2: bool = False
+            bit3: bool = True
+            bit4: bool = False
+            bit5: bool = True
+            bit6: bool = False
+            bit7: bool = True
+
+        # Act
+        reg = TestRegister()
+        integer = reg.to_bytes()
+
+        # Assert
+        assert integer == b"\xAA"
+
+    def test_bitwiseReg16_from_bytes(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg16):
+            """register dataclass"""
+
+            bit0: bool
+            bit1: bool
+            bit2: bool
+            bit3: bool
+            bit4: bool
+            bit5: bool
+            bit6: bool
+            bit7: bool
+            bit8: bool
+            bit9: bool
+            bit10: bool
+            bit11: bool
+            bit12: bool
+            bit13: bool
+            bit14: bool
+            bit15: bool
+
+        # Act
+        reg = TestRegister.from_bytes(b"\xAA\xFF")
+
+        # Assert
+        reg.bit0 = True
+        reg.bit1 = False
+        reg.bit2 = True
+        reg.bit3 = False
+        reg.bit4 = True
+        reg.bit5 = False
+        reg.bit6 = True
+        reg.bit7 = False
+        reg.bit8 = True
+        reg.bit9 = True
+        reg.bit10 = True
+        reg.bit11 = True
+        reg.bit12 = True
+        reg.bit13 = True
+        reg.bit14 = True
+        reg.bit15 = True
+
+    def test_bitwiseReg16_from_int(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg16):
+            """register dataclass"""
+
+            bit0: bool
+            bit1: bool
+            bit2: bool
+            bit3: bool
+            bit4: bool
+            bit5: bool
+            bit6: bool
+            bit7: bool
+            bit8: bool
+            bit9: bool
+            bit10: bool
+            bit11: bool
+            bit12: bool
+            bit13: bool
+            bit14: bool
+            bit15: bool
+
+        # Act
+        reg = TestRegister.from_int(0xAAFF)
+
+        # Assert
+        reg.bit0 = True
+        reg.bit1 = False
+        reg.bit2 = True
+        reg.bit3 = False
+        reg.bit4 = True
+        reg.bit5 = False
+        reg.bit6 = True
+        reg.bit7 = False
+        reg.bit8 = True
+        reg.bit9 = True
+        reg.bit10 = True
+        reg.bit11 = True
+        reg.bit12 = True
+        reg.bit13 = True
+        reg.bit14 = True
+        reg.bit15 = True
+
+    def test_bitwiseReg16_to_int(self):
+        "Test bitwiseReg functions"
+
+        # Arrange
+        @dataclass
+        class TestRegister(CpxBase.BitwiseReg16):
+            """register dataclass"""
+
+            bit0: bool = False
+            bit1: bool = True
+            bit2: bool = False
+            bit3: bool = True
+            bit4: bool = False
+            bit5: bool = True
+            bit6: bool = False
+            bit7: bool = True
+            bit8: bool = True
+            bit9: bool = True
+            bit10: bool = True
+            bit11: bool = True
+            bit12: bool = True
+            bit13: bool = True
+            bit14: bool = True
+            bit15: bool = True
+
+        # Act
+        reg = TestRegister()
+        integer = reg.to_bytes()
+
+        # Assert
+        assert integer == b"\xAA\xFF"
+
+    def test_read_reg_data_without_length(self):
+        "Test read_reg_data function"
+
+        # Arrange
+        class response:
+            """mock response object"""
+
+            def __init__(self):
+                self.registers = [0]
+
+            def isError(self):
+                "mock error function"
+                return False
+
+        cpx = CpxBase()
+        cpx.client = Mock(read_holding_registers=Mock(return_value=response()))
+
+        # Act
+        data = cpx.read_reg_data(0)
+
+        # Assert
+        assert data == b"\x00\x00"
+
+    def test_read_reg_data_with_length(self):
+        "Test read_reg_data function"
+
+        # Arrange
+        class response:
+            """mock response object"""
+
+            def __init__(self):
+                self.registers = [0, 1, 2, 3]
+
+            def isError(self):
+                "mock error function"
+                return False
+
+        cpx = CpxBase()
+        cpx.client = Mock(read_holding_registers=Mock(return_value=response()))
+
+        # Act
+        data = cpx.read_reg_data(0, 4)
+
+        # Assert
+        assert data == b"\x00\x00\x01\x00\x02\x00\x03\x00"
+
+    def test_read_reg_data_error(self):
+        "Test read_reg_data function"
+
+        # Arrange
+        class response:
+            """mock response object"""
+
+            def __init__(self):
+                self.registers = [0, 1, 2, 3]
+                self.exception_code = "test"
+
+            def isError(self):
+                "mock error function"
+                return True
+
+        cpx = CpxBase()
+        cpx.client = Mock(read_holding_registers=Mock(return_value=response()))
+
+        # Act & Assert
+        with pytest.raises(ConnectionAbortedError):
+            cpx.read_reg_data(0)
+
+    @pytest.mark.parametrize(
+        "input_value, expected_value",
+        [
+            (b"\xAA", [0xAA]),
+            (b"\xAA\xBB", [0xBBAA]),
+            (b"\xAA\xBB\xCC", [0xBBAA, 0xCC]),
+            (b"\xAA\xBB\xCC\xDD", [0xBBAA, 0xDDCC]),
+        ],
+    )
+    def test_write_reg_data_nBytes(self, input_value, expected_value):
+        "Test write_reg_data function"
+
+        # Arrange
+        cpx = CpxBase()
+        cpx.client = Mock(write_registers=Mock())
+
+        # Act
+        cpx.write_reg_data(input_value, 0)
+
+        # Assert
+        cpx.client.write_registers.assert_called_with(0, expected_value)
+
+    def test_require_base_missing(self):
+        "Test require_base function"
+
+        class testClass(CpxBase):
+
+            @CpxBase.require_base
+            def testFunction(self):
+                return True
+
+        # Arrange
+        cpx = testClass()
+
+        # Act & Assert
+        with pytest.raises(CpxInitError):
+            cpx.testFunction()
+
+    def test_require_base_ok(self):
+        "Test require_base function"
+
+        class testClass(CpxBase):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.base = CpxBase()
+
+            @CpxBase.require_base
+            def testFunction(self):
+                return True
+
+        # Arrange
+        cpx = testClass()
+
+        # Act & Assert
+        assert cpx.testFunction()
