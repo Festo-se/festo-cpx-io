88af80e6d8f63f9e2f70d7efaaa42bedf21bd1ce
maws <martin.wiesner@festo.com>
2024-08-22 09:40:49 +0200

    Bugfix for uint8 write

--- src/cpx_io/cpx_system/cpx_ap/ap_module.py
+++ src/cpx_io/cpx_system/cpx_ap/ap_module.py
@@ -635,9 +635,9 @@ class ApModule(CpxModule):
             reg = self.base.read_reg_data(self.output_register)
             # if channel number is odd, value needs to be stored in the MSByte
             if channel % 2:
-                reg = struct.pack("<b", value) + reg[:1]
-            else:
                 reg = reg[1:] + struct.pack("<b", value)
+            else:
+                reg = struct.pack("<b", value) + reg[:1]
 
             self.base.write_reg_data(reg, self.output_register)
             Logging.logger.info(
@@ -652,9 +652,10 @@ class ApModule(CpxModule):
             reg = self.base.read_reg_data(self.output_register)
             # if channel number is odd, value needs to be stored in the MSByte
             if channel % 2:
-                reg = struct.pack("<B", value) + reg[:1]
-            else:
                 reg = reg[1:] + struct.pack("<B", value)
+            else:
+                reg = struct.pack("<B", value) + reg[:1]
+
             self.base.write_reg_data(reg, self.output_register)
             Logging.logger.info(
                 f"{self.name}: Setting uint8 channel {channel} to {value}"
