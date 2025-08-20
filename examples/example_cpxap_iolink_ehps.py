7749378aca5b6473644dca2e2c082afa39a82779
Jan Hildenbrand <jan.hildenbrand@festo.com>
2025-08-20 10:02:48 +0200

    Update example to fit CPX-AP

--- examples/example_cpxap_iolink_ehps.py
+++ examples/example_cpxap_iolink_ehps.py
@@ -12,9 +12,7 @@ def read_process_data_in(data):
     """Read the process data and return as dict"""
     # ehps provides 3 x UIntegerT16 "process data in" according to datasheet.
     # you can unpack the data easily with stuct
-    ehps_data = struct.unpack(">HHHH", data)
-    # the last 16 bit value of this list is always 0 since ehps only uses 3*16 bit
-    assert ehps_data[3] == 0
+    ehps_data = struct.unpack(">HHH", data)
 
     process_data_dict = {}
 
@@ -38,29 +36,22 @@ def read_process_data_in(data):
 # list of some connected modules. IO-Link module is specified with 8 bytes per port:
 # Datasheet: Per port: 8 E / 8 A  Module: 32 E / 32 A. This has to be set up with the
 # switch on the module.
-with CpxAp(ip_address="192.168.1.1") as myCPX:
-
+with CpxAp(ip_address="172.16.1.41") as myCPX:
     # example EHPS-20-A-LK on port 1
     EHPS_CHANNEL = 1
 
     # (optional) get the module and assign it to a new variable for easy access
-    io_link_master = myCPX.modules[4]
-
-    # (optional) you can reset the power for each channel
-    io_link_master.configure_pl_supply(False, EHPS_CHANNEL)
-    io_link_master.configure_ps_supply(False)
-    time.sleep(0.05)
-    io_link_master.configure_pl_supply(True, EHPS_CHANNEL)
-    io_link_master.configure_ps_supply(True)
-    time.sleep(0.05)
+    io_link_master = myCPX.modules[5]
 
     # set operating mode of channel 0 to "IO-Link communication"
-    io_link_master.configure_operating_mode(3, channel=EHPS_CHANNEL)
+    io_link_master.write_module_parameter("Port Mode", "IOL_AUTOSTART", EHPS_CHANNEL)
 
     time.sleep(0.05)
 
     # (optional) read line-state, should now be "OPERATE" for the channel
-    param = io_link_master.read_line_state()
+    param = io_link_master.read_fieldbus_parameters()
+    while param[EHPS_CHANNEL]["Port status information"] != "OPERATE":
+        param = io_link_master.read_fieldbus_parameters()
 
     # read process data
     raw_data = io_link_master.read_channel(EHPS_CHANNEL)
@@ -69,30 +60,31 @@ with CpxAp(ip_address="192.168.1.1") as myCPX:
     process_data_in = read_process_data_in(raw_data)
 
     # demo of process data out needed to initialize EHPS
-    control_word_msb = 0x00
-    control_word_lsb = 0x01  # latch
+    control_word = 0x0001  # latch
     gripping_mode = 0x46  # universal
     workpiece_no = 0x00
     gripping_position = 0x03E8
     gripping_force = 0x03  # ca. 85%
     gripping_tolerance = 0x0A
 
-    data = [
-        control_word_lsb + (control_word_msb << 8),
-        workpiece_no + (gripping_mode << 8),
+    pd_list = [
+        control_word,
+        gripping_mode,
+        workpiece_no,
         gripping_position,
-        gripping_tolerance + (gripping_force << 8),
+        gripping_force,
+        gripping_tolerance,
     ]
 
+
     # init
-    # pack the 4*16bit values to bytes
-    process_data_out = struct.pack(">HHHH", *data)
+    process_data_out = struct.pack(">HBBHBB", *pd_list)
     io_link_master.write_channel(EHPS_CHANNEL, process_data_out)
     time.sleep(0.05)
 
     # Open command: 0x0100
-    process_data_out[0] = 0x0100
-    process_data_out = struct.pack(">HHHH", *data)
+    pd_list[0] = 0x0100
+    process_data_out = struct.pack(">HBBHBB", *pd_list)
     io_link_master.write_channel(EHPS_CHANNEL, process_data_out)
 
     # wait for the process data in to change to "opened"
@@ -101,11 +93,17 @@ with CpxAp(ip_address="192.168.1.1") as myCPX:
         time.sleep(0.05)
 
     # Close command 0x 0200
-    process_data_out[0] = 0x0200
-    process_data_out = struct.pack(">HHHH", *data)
+    pd_list[0] = 0x0200
+    process_data_out = struct.pack(">HBBHBB", *pd_list)
     io_link_master.write_channel(EHPS_CHANNEL, process_data_out)
 
     # wait for the process data in to change to "closed"
     while not process_data_in["ClosedPositionFlag"]:
         process_data_in = read_process_data_in(io_link_master.read_channel(EHPS_CHANNEL))
         time.sleep(0.05)
+
+    io_link_master.write_module_parameter("Port Mode", "DEACTIVATED", EHPS_CHANNEL)
+    # wait for inactive
+    param = io_link_master.read_fieldbus_parameters()
+    while param[EHPS_CHANNEL]["Port status information"] != "DEACTIVATED":
+        param = io_link_master.read_fieldbus_parameters()
